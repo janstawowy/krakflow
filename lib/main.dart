733e0ab63260c5e2c6a919430e70ed039ca304b1
jan <stawa29@interia.pl>
2026-05-16 17:13:42 +0200

    lab_05 restapi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'task_repository.dart';
+import 'task_api_service.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -32,24 +34,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late Future<List<Task>> tasksFuture;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    tasksFuture = TaskApiService.fetchTasks();
   }
-  // List<Task> tasks = [
-  //   Task(title: "zrobic zadania z TAM", deadline: "31.03.2026", done: false, priority: "High"),
-  //   Task(title: "Call z supportem mikromiekkich", deadline: "dzisiaj", done: true, priority: "Medium"),
-  //   Task(title: "zrobic taski z jiry", deadline: "w tym tygodniu", done: false, priority: "Critical"),
-  //   Task(title: "Zjesc obiad", deadline: "dzisiaj", done: true, priority: "Low"),
-  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -60,43 +51,69 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              "Masz dzisiaj ${TaskRepository.tasks.length} zadania.\nWykonanych: $completed_tasks",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              "Dzisiejsze zadania:",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
+      body: FutureBuilder<List<Task>>(
+        future: tasksFuture,
 
-          Expanded(
-            child: ListView.builder(
-              itemCount: TaskRepository.tasks.length,
-              itemBuilder: (context, index) {
-                return TaskCard(
-                  title: TaskRepository.tasks[index].title,
-                  subtitle: "${TaskRepository.tasks[index].deadline} | Priorytet: ${TaskRepository.tasks[index].priority}",
-                  icon: TaskRepository.tasks[index].done ? Icons.check_circle : Icons.radio_button_unchecked,
-                );
-              },
-            ),
-          ),
-        ],
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Błąd: ${snapshot.error}"),
+            );
+          }
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+
+          final tasks = snapshot.data!;
+
+          int completedTasks =
+              tasks.where((task) => task.done).length;
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  "Masz dzisiaj ${tasks.length} zadania.\n"
+                      "Wykonanych: $completedTasks",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  "Dzisiejsze zadania:",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index) {
+                    return TaskCard(
+                      title: tasks[index].title,
+                      subtitle: "${tasks[index].deadline} | Priorytet: ${tasks[index].priority}",
+                      icon: tasks[index].done ? Icons.check_circle : Icons.radio_button_unchecked,
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
---
+++ lib/task_api_service.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'task_repository.dart';
+import 'dart:math';
+final random = Random();
+final priorities = ["niski", "średni", "wysoki"];
+final deadlines = ["teraz", "jutro", "za tydzien", "tak"];
+final priority = priorities[random.nextInt(priorities.length)];
+final deadline = deadlines[random.nextInt(deadlines.length)];
+
+class TaskApiService {
+  static const String baseUrl = "https://dummyjson.com/";
+  static Future<List<Task>> fetchTasks() async {
+    final response = await http.get(
+        Uri.parse("$baseUrl/todos")
+    );
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final List tasks = data["todos"];
+      return tasks.map((todo) {
+        return Task(
+          title: todo["todo"],
+          deadline: deadlines[random.nextInt(deadlines.length)],
+          done: todo["completed"],
+          priority: priorities[random.nextInt(priorities.length)],
+        );
+      }).toList();
+    } else {
+      throw Exception("Błąd pobierania danych");
+    }
+  }
+}
